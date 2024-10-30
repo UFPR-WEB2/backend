c530b2f685976d7a7b84d2c1c19d07018e341445
Gabriel Henrique Speraceta <gabriel.hesperanceta@gmail.com>
2024-10-30 18:45:14 -0300

    HomeTeste

--- src/main/java/com/grupo2/demo/controller/HomeController.java
+++ src/main/java/com/grupo2/demo/controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.grupo2.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("mensagem", "Bem-vindo ao backend com MVC!");
-        return "index";
+    @GetMapping("/teste")
+    public String home() {
+        return "Bem-vindo ao backend!";
     }
 }

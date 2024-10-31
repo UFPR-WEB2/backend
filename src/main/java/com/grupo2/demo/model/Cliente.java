43b40c17943d94ba6ced361a2fe21795a18532b5
Bruno_Brug <brunobrugnerottodelara@gmail.com>
2024-10-31 10:49:13 -0300

    teste

--- src/main/java/com/grupo2/demo/model/Cliente.java
+++ src/main/java/com/grupo2/demo/model/Cliente.java
@@ -1,15 +1,29 @@
 package com.grupo2.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class Cliente {
 
+    @Id
+    @GenerateValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
+    private String cpf;
+    private String endereco;
+    private String telefone;
 
-    public Cliente(Long id, String nome, String email) {
+    public Cliente(Long id, String nome, String email, String cpf, String endereco, String telefone) {
         this.id = id;
         this.nome = nome;
         this.email = email;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
     }
 
     public Long getId() {
@@ -36,5 +50,27 @@ public class Cliente {
         this.email = email;
     }
 
-    
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }

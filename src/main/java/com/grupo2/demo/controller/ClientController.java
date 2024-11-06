49e33e4163adb679997c55fbf7cc4bb91ccf80c4
Bruno_Brug <brunobrugnerottodelara@gmail.com>
2024-11-06 20:20:56 -0300

    [ADD] metodos REST para ClientController e FixController

--- src/main/java/com/grupo2/demo/controller/ClientController.java
+++ src/main/java/com/grupo2/demo/controller/ClientController.java
@@ -1,13 +1,12 @@
 package com.grupo2.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import com.grupo2.demo.model.Client;
 import com.grupo2.demo.repository.ClientRepository;
 
-
-// ADICIONEM METODOS REST PARA O CONTROLLER
 @RestController
 @RequestMapping("/api/cliente")
 public class ClientController {
@@ -17,13 +16,44 @@ public class ClientController {
 
     @GetMapping
     public List<Client> listarClientes() {
-        List<Client> usuarios = new ArrayList<>();
-
-        return usuarios;
+        return clientRepository.findAll();
     }
 
     @PostMapping
     public Client criarCliente(@RequestBody Client usuario) {
         return clientRepository.save(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> obterClientePorId(@PathVariable Long id) {
+        Optional<Client> cliente = clientRepository.findById(id);
+        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> atualizarCliente(@PathVariable Long id, @RequestBody Client detalhesCliente) {
+        Optional<Client> clienteOptional = clientRepository.findById(id);
+
+        if (clienteOptional.isPresent()) {
+            Client cliente = clienteOptional.get();
+            cliente.setNome(detalhesCliente.getNome());
+            cliente.setEmail(detalhesCliente.getEmail());
+            cliente.setTelefone(detalhesCliente.getTelefone());
+
+            Client clienteAtualizado = clientRepository.save(cliente);
+            return ResponseEntity.ok(clienteAtualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
+        if (clientRepository.existsById(id)) {
+            clientRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }

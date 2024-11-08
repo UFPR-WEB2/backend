3075067c31d21d80bcd57a92f0aae18c8070aa51
Gabriel Henrique Speraceta <gabriel.hesperanceta@gmail.com>
2024-11-08 16:05:15 -0300

    [Config]

--- src/main/java/com/grupo2/demo/service/AuthService.java
+++ src/main/java/com/grupo2/demo/service/AuthService.java
@@ -4,16 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.grupo2.demo.model.User.Costumer;
 import com.grupo2.demo.repository.CostumerRepository;
+import jakarta.servlet.http.HttpSession;
 
 @Service
 public class AuthService {
     @Autowired
     private CostumerRepository clientRepository;
 
+    @Autowired
+    private HttpSession session;
+
     public boolean authenticate(String username, String password) {
         Costumer client = clientRepository.findByNome(username);
         if(client != null) {
-            return client.getPassword().equals(password);
+            if(client.getPassword().equals(password)) {
+                session.setAttribute("user", client);
+                return true;
+            }
         }
         return false;
     }

7632822d060d30d5c0490e994f08512324e31862
voidalzk <voidalzk@gmail.com>
2024-11-07 23:21:56 -0300

    [add] complete MaintenanceResponsible Model

--- src/main/java/com/grupo2/demo/model/Maintenance/MaintenanceResponsible.java
+++ src/main/java/com/grupo2/demo/model/Maintenance/MaintenanceResponsible.java
@@ -1,5 +1,45 @@
 package com.grupo2.demo.model.Maintenance;
 
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class MaintenanceResponsible {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate data_redirecionamento;
+
+    @Column(nullable = false)
+    private String Status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getData_redirecionamento() {
+        return data_redirecionamento;
+    }
+
+    public void setData_redirecionamento(LocalDate data_redirecionamento) {
+        this.data_redirecionamento = data_redirecionamento;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
 }
--- src/main/java/com/grupo2/demo/service/AuthService.java
+++ src/main/java/com/grupo2/demo/service/AuthService.java
@@ -2,14 +2,8 @@ package com.grupo2.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
 import com.grupo2.demo.model.User.Costumer;
 import com.grupo2.demo.repository.CostumerRepository;
-=======
-
-import com.grupo2.demo.model.User.Customer;
-import com.grupo2.demo.repository.ClientRepository;
->>>>>>> e24476d9dc39a43b8d0b4952cfa065957f67639d
 
 @Service
 public class AuthService {
@@ -17,11 +11,7 @@ public class AuthService {
     private CostumerRepository clientRepository;
 
     public boolean authenticate(String username, String password) {
-<<<<<<< HEAD
         Costumer client = clientRepository.findByNome(username);
-=======
-        Customer client = clientRepository.findByNome(username);
->>>>>>> e24476d9dc39a43b8d0b4952cfa065957f67639d
         if(client != null) {
             return client.getPassword().equals(password);
         }

e10bba62db1c70cfad122db830ca7139433c6ab3
Gabriel Henrique Speraceta <gabriel.hesperanceta@gmail.com>
2024-11-17 19:06:44 -0300

    [FIX] Logica de retorno de metodos

--- src/main/java/com/grupo2/demo/model/Maintenance/MaintenanceResponsible.java
+++ src/main/java/com/grupo2/demo/model/Maintenance/MaintenanceResponsible.java
@@ -1,11 +1,16 @@
 package com.grupo2.demo.model.Maintenance;
 
 import java.time.LocalDate;
+
+import com.grupo2.demo.model.User.Employee;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class MaintenanceResponsible {
@@ -18,6 +23,10 @@ public class MaintenanceResponsible {
 
     @Column(nullable = false)
     private String Status;
+
+    @ManyToOne
+    @JoinColumn(name = "id_funcionario", nullable = false)
+    private Employee funcionario;
 
     public Long getId() {
         return id;
@@ -42,4 +51,14 @@ public class MaintenanceResponsible {
     public void setStatus(String Status) {
         this.Status = Status;
     }
+
+    public Employee getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Employee funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    
 }

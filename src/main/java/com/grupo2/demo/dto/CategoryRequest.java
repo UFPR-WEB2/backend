feb8c5c91a764f39c26bcb3ef7d145c03ab1c3f2
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2024-11-21 10:58:04 -0300

    add: atualizacao de Category para nova arquitetura (controller, dto's e service)

---
+++ src/main/java/com/grupo2/demo/dto/CategoryRequest.java
@@ -0,0 +1,13 @@
+package com.grupo2.demo.dto;
+
+public class CategoryRequest {
+    private String nomeCategoria;
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+}
